2dcd0742ddded83373627a5d745a93e8058af15d
raouaagara <raouaagara@gmail.com>
2026-05-06 19:45:03 +0100

    fix: remove BOM from MatchyApplication.java

--- src/main/java/com/matchy/MatchyApplication.java
+++ src/main/java/com/matchy/MatchyApplication.java
@@ -1,4 +1,4 @@
-﻿package com.matchy;
+package com.matchy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
